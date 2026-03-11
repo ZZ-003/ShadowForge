c322b7a75ce42cadd6ba122f3bdb8b26325abec9
ConstMoissanite <3051376580@qq.com>
2026-03-11 15:08:26 +0000

    refactor: light renaming for consistency and easiness to call

--- generators/secret_generators/apiKey_gen.py
+++ generators/secret_generators/apiKey_gen.py
@@ -5,13 +5,13 @@ from enum import Enum
 class Platform(Enum):
     OpenAI_Personal = 1
     Deepseek        = 2
-    BaiLian         = 3     #Aliyun
+    Bailian         = 3     #Aliyun
     OpenAI_Project  = 4     #To do
     Moonshot        = 5
     HuggingFace     = 6
     #To be extended
 
-class KeyRandGen:
+class APIKeyRandGen:
     
     def Deepseek_keygen(self) -> str :
         return 'sk-'+secrets.token_hex(16)
@@ -37,15 +37,15 @@ class KeyRandGen:
         payload:str=''.join(secrets.choice(randset) for _ in range(34))
         return 'hf_'+payload
 
-def keygen(modeset:int) -> str:
-    Generator=KeyRandGen()
+def APIkeygen(modeset:int) -> str:
+    Generator=APIKeyRandGen()
     match Platform(modeset):
         case Platform.OpenAI_Personal:
             return Generator.OpenAI_Personal_keygen()
         case Platform.Deepseek:
             return Generator.Deepseek.keygen()
         case Platform.BaiLian:
-            return Generator.Balian_keygen()
+            return Generator.Bailian_keygen()
         case Platform.Moonshot:
             return Generator.Moonshot_keygen()
         case Platform.OpenAI_Project:
@@ -53,8 +53,3 @@ def keygen(modeset:int) -> str:
         case Platform.HuggingFace:
             return Generator.HuggingFace_keygen()
         
-# Test Only
-if __name__ == "__main__":
-    print("mode: ",end='')
-    mode=int(input())
-    print(keygen(mode))
