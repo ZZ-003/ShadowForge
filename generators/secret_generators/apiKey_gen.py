fe17e7d97e4ff0e9939cba95d957ccc7fd7df1e7
ConstMoissanite <3051376580@qq.com>
2026-03-11 03:10:55 +0000

    feat:supporting multi-platform API_key Generating for fake secrets

---
+++ generators/secret_generators/apiKey_gen.py
@@ -0,0 +1,47 @@
+import secrets
+import string
+from enum import Enum
+
+class Platform(Enum):
+    OpenAI_Personal = 1
+    DeepSeek        = 2
+    BaiLian         = 3     #Aliyun
+    OpenAI_Project  = 4     #To do
+    Moonshot        = 5
+    #To be extended
+
+class KeyRandGen:
+    
+    def Deepseek_keygen(self) -> str :
+        return 'sk-'+secrets.token_hex(16)
+
+    def Bailian_keygen(self) -> str:
+        return 'sk-'+secrets.token_hex(16)
+
+    def OpenAI_Personal_keygen(self) -> str:
+        randset = string.ascii_letters + string.digits
+        payload:str=''.join(secrets.choice(randset) for _ in range(48))
+        return 'sk-'+payload
+
+    def Moonshot_keygen(self) -> str:
+        randset = string.ascii_letters + string.digits
+        payload:str=''.join(secrets.choice(randset) for _ in range(48))
+        return 'sk-'+payload
+
+    def OpenAI_Project_keygen(self) -> str:
+        pass
+    
+
+def keygen(modeset:int) -> str:
+    Generator=KeyRandGen()
+    match Platform(modeset):
+        case Platform.OpenAI_Personal:
+            return Generator.OpenAI_Personal_keygen()
+        case Platform.Deepseek:
+            return Generator.Deepseek.keygen()
+        case Platform.Bailian:
+            return Generator.Balian_keygen()
+        case Platform.Moonshot:
+            return Generator.Moonshot_keygen()
+        case Platform.OpenAI_Project:
+            pass
