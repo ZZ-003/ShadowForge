51a73751696d4d0d0e86fd57df7cd9bdc3a2e873
ConstMoissanite <3051376580@qq.com>
2026-03-16 16:03:33 +0800

    feat&refactor:renaming file; adding AWS /Github support

--- generators/secret_generators/Key_Gen.py
+++ generators/secret_generators/Key_Gen.py
@@ -9,6 +9,9 @@ class Platform(Enum):
     OpenAI_Project  = 4     #To do
     Moonshot        = 5
     HuggingFace     = 6
+    AWSAccess       = 7
+    Github          = 8
+    OpenSSH         = 9
     #To be extended
 
 class APIKeyRandGen:
@@ -30,12 +33,39 @@ class APIKeyRandGen:
         return 'sk-'+payload
 
     def OpenAI_Project_keygen(self) -> str:
-        pass
+        return ''
     
     def HuggingFace_keygen(self) -> str:
         randset=string.ascii_letters
         payload:str=''.join(secrets.choice(randset) for _ in range(34))
         return 'hf_'+payload
+
+    def AWSAccess_keygen(self) -> str:
+        randset0=string.ascii_uppercase+ string.digits
+        randpreset=['AKIA','ASIA','ABIA','ACCA','AGPA']
+        randkeyset=string.ascii_letters+string.digits+'/+='
+        prefix1=secrets.choice(randpreset)
+        prefix2=''.join(secrets.choice(randset0) for _ in range(16))
+        keystr:str=prefix1+prefix2+''.join(secrets.choice(randkeyset) for _ in range(40))
+        return keystr
+    
+    def Github_keygen(self) -> str:
+        prefix:str='ghp+'
+        randset=string.ascii_letters+string.digits
+        payload:str=''.join(secrets.choice(randset) for _ in range(36))
+        keystr:str=prefix+payload
+        return keystr
+    
+    def OpenSSH_keygen(self) -> str:
+        prefix:str='-----BEGIN OPENSSH PRIVATE KEY-----\n'
+        end='\n-----END OPENSSH PRIVATE KEY-----\n'
+        keystr=''
+        return keystr
+        
+
+
+
+
 
 def APIkeygen(modeset:int) -> str:
     Generator=APIKeyRandGen()
@@ -43,13 +73,19 @@ def APIkeygen(modeset:int) -> str:
         case Platform.OpenAI_Personal:
             return Generator.OpenAI_Personal_keygen()
         case Platform.Deepseek:
-            return Generator.Deepseek.keygen()
-        case Platform.BaiLian:
+            return Generator.Deepseek_keygen()
+        case Platform.Bailian:
             return Generator.Bailian_keygen()
         case Platform.Moonshot:
             return Generator.Moonshot_keygen()
         case Platform.OpenAI_Project:
-            pass
+            return Generator.OpenAI_Project_keygen()
         case Platform.HuggingFace:
             return Generator.HuggingFace_keygen()
+        case Platform.AWSAccess:
+            return Generator.AWSAccess_keygen()
+        case Platform.Github:
+            return Generator.Github_keygen()
+        case default:
+            raise ValueError('Invalid Platform Index')
         
